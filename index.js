251f9752122d089943ffa610ef2999b02a0ac534
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-02-05 21:29:35 +0500

    feat: add print username

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("Hello World!");
+const args = process.argv.slice(2);
+const username = args.find(arg => arg.includes("--username")).split("=")[1];
+console.log(`Welcome to the File Manager, ${username}!`);
