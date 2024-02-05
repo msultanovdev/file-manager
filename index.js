4a9f685c7dcd6ce70ee882207e0428a35ebcea61
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-02-05 21:53:17 +0500

    feat: add user promt & exit message

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+import {rl} from "./utils/helper.js";
 const args = process.argv.slice(2);
 const username = args.find(arg => arg.includes("--username")).split("=")[1];
 console.log(`Welcome to the File Manager, ${username}!`);
+
+function handleCommand(command) {
+    switch (command) {
+      case ".exit":
+      case "exit":
+        console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+        process.exit(0);
+        break;
+      default:
+        console.log(`Command received: ${command}`);
+  }
+}
+
+async function userInput() {
+    const command = await new Promise((resolve) =>
+      rl.question("> ", resolve)
+    );
+    handleCommand(command);
+    userInput();
+}
+  
+process.on("SIGINT", () => {});
+  
+process.on("beforeExit", () => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+});
+
+userInput();
+  
